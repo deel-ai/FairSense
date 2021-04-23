3fcbd4984d47c7fab5bd5e7fc0cf2a74fb0fc9b7
Quentin Vincenot <quentin.vincenot@thalesaleniaspace.com>
2021-04-23 10:42:11 +0100

    Update and cleanup of BDE example with CVM indices

--- datasets.py
+++ datasets.py
@@ -1,5 +1,9 @@
-import sklearn.datasets as ds
 from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from torch.utils.data import Dataset, DataLoader
+import pandas as pd
+import sklearn.datasets as ds
+import torch
 
 
 class Datasets:
@@ -41,3 +45,51 @@ class Datasets:
         data = ds.load_boston()
         # Return the list of the dataset features
         return data.feature_names
+
+
+
+    @staticmethod
+    def load_Breaking_Distance_Estimation():
+        # Define both BDE file paths
+        INPUT_PATH = "./data_BDE/"
+        input_file = "%sDATA_GENERIC.csv" % INPUT_PATH
+        input_file_test = "%sDATA_GENERIC_VALIDATION.csv" % INPUT_PATH
+        # Load both files from the BDE dataset into Pandas dataframes
+        df = pd.read_csv(input_file, delimiter=";")
+        df_test = pd.read_csv(input_file_test, delimiter=";")
+        # Concatenate both dataframes into a single one
+        df = pd.concat([df, df_test])
+        # One-hot encode the "state" input column
+        df_dummies = pd.get_dummies(df["Input_08"])
+        df_global = pd.concat([df.drop(['OUTPUT', 'Input_08'], axis = 1), df_dummies], axis=1, join_axes=[df.index])
+        # Use standard scaler to scale real-valued input columns
+        fit_colnames = ['Input_01', 'Input_02', 'Input_03', 'Input_04', 'Input_05', 'Input_06']
+        scaler = StandardScaler()
+        scaler.fit(df_global[fit_colnames])
+        # Split the dataset into training and validation sets
+        df_train, df_test, y_train, y_test = train_test_split(df_global, df['OUTPUT'], test_size=0.4, random_state=42)
+        df_test, df_val, y_test, y_val = train_test_split(df_test, y_test, test_size=0.5, random_state=42)
+        # Create a PyTorch dataloader from the training dataframe
+        bs = 10000
+        class DataSetRops(Dataset):
+            def __init__(self, X, y):
+                if not torch.is_tensor(X):
+                    self.X = torch.tensor(X.values, dtype=torch.float64)
+                if not torch.is_tensor(y):
+                    self.y = torch.tensor(y.values, dtype=torch.float64)
+            def __len__(self):
+                return len(self.X)
+            def __getitem__(self, idx):
+                return self.X[idx], self.y[idx]
+        data_torch = DataSetRops(df_train, y_train)
+        dataloader = DataLoader(data_torch, batch_size=bs,shuffle=True)
+        return dataloader, scaler, fit_colnames, df_train, y_train, df_val, y_val, df_test, y_test
+
+
+
+
+
+
+
+
+
--- methods.py
+++ methods.py
@@ -1,7 +1,9 @@
 from sobol_indices.dataset_analyser import analyze
-import pandas as pd
-import numpy as np
+from CVM_indices.CVM_draft import analyze
 import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import os
 
 
 class SobolIndices:
@@ -61,17 +63,95 @@ class SobolIndices:
                 val = pct * np.sum(values) / 100.
                 return "({:.2f})".format(val)
             return auto_pct
+        # Retrieve main Sobol indices
         sobol_values = sobol_indices[['S']].values.squeeze()
         sobol_total_values = sobol_indices[['ST']].values.squeeze()
         sobol_ind_values = sobol_indices[['S_ind']].values.squeeze()
         sobol_total_ind_values = sobol_indices[['ST_ind']].values.squeeze()
         fig, axs = plt.subplots(2, 2, figsize=(16, 16))
+        # Display a piechart for Sobol indices
         axs[0, 0].set_title('Sobol')
         axs[0, 0].pie(sobol_values, labels=columns_names, autopct=label_pie(sobol_values), shadow=False, normalize=True)
+        # Display a piechart for Sobol Total indices
         axs[0, 1].set_title('Sobol Total')
         axs[0, 1].pie(sobol_total_values, labels=columns_names, autopct=label_pie(sobol_total_values), shadow=False, normalize=True)
+        # Display a piechart for Sobol independent indices
         axs[1, 0].set_title('Sobol independent')
         axs[1, 0].pie(sobol_ind_values, labels=columns_names, autopct=label_pie(sobol_ind_values), shadow=False, normalize=True)
+        # Display a piechart for Sobol Total independent indices
         axs[1, 1].set_title('Sobol Total independent')
         axs[1, 1].pie(sobol_total_ind_values, labels=columns_names, autopct=label_pie(sobol_total_ind_values), shadow=False, normalize=True)
         plt.show()
+
+
+
+class CVMIndices:
+    
+    @staticmethod
+    def compute(dataframe, ouput):
+        # Create a deep copy of the input dataframe (to not destroy/modify data)
+        df = dataframe.copy(deep=True)
+        # Concatenate the output column in the dataframe
+        df['OUTPUT'] = ouput
+        # Build the columns list and remove the output column
+        list_of_columns = [[c] for c in df.columns]
+        list_of_columns.remove(["OUTPUT"])
+        # Add a defined epsilon to categorical columns
+        eps = 1e-5
+        # Handle categorical columns
+        categorical_columns = ["Input_07"]
+        for cat in categorical_columns:
+            # extract the column and remove it from original dataset
+            cat_serie = df[cat]
+            df = df.drop(cat, axis=1)
+            # perform one_hot encoding
+            # take care to remove the first one hot encoded vector in order to avoid bias
+            dum_cat_serie = pd.get_dummies(cat_serie, prefix="{}".format(cat), prefix_sep="=", drop_first=False)
+            # add random to break ties
+            dum_cat_serie = dum_cat_serie + pd.np.random.uniform(-eps, eps, dum_cat_serie.shape)
+            # put it back in the dataframe
+            df = pd.concat([df, dum_cat_serie], axis=1)
+            # remove the column name and replace it with the list of newly created columns
+            list_of_columns.remove([cat])
+            list_of_columns.append(dum_cat_serie.columns.to_list())
+        # Perform the computation of the CVM indices, with respect to the output column
+        indices = analyze(df.sample(10000), 'OUTPUT', list_of_columns)
+        return indices
+
+    @staticmethod
+    def visualize_barcharts(cvm_indices):
+        # Retrieve xticks, cvm indices and cvm independent indices for further plotting
+        columns = cvm_indices.index.tolist()
+        cvm_series = cvm_indices['CVM'].tolist()
+        cvm_independent_series = cvm_indices['CVM_indep'].tolist()
+        # Compute xticks location and set the width of the bars
+        x = np.arange(len(columns))
+        width = 0.15
+        # Initialize the plot
+        fig, ax = plt.subplots(figsize=(13, 6))
+        # Display both bars at each tick location for both indices
+        rects1 = ax.bar(x - width/2, cvm_series, width, label='CVM')
+        rects2 = ax.bar(x + width/2, cvm_independent_series, width, label='CVM independent')
+        # Display a dedicated horizontal dashed line for 1.0 value (higher limit of indices)
+        ax.plot([0, len(columns)-1], [1., 1.], '--r', linewidth=0.5)
+        # Add some text for labels, title and custom x-axis tick labels, etc.
+        ax.set_ylabel('Index value')
+        ax.set_title('CVM/CVM independent indices')
+        ax.set_xticks(x)
+        ax.set_xticklabels(columns)
+        ax.set_ylim([0.0, 1.15])
+        # Annotate values of CVM indices above bars
+        for idx, cvm_value in enumerate(cvm_series):
+            rounded_cvm = round(cvm_value, 3)
+            ax.annotate(rounded_cvm, xy=(idx-0.14, cvm_value+0.05), rotation=90)
+        # Annotate values of CVM independent indices above bars
+        for idx, cvm_ind_value in enumerate(cvm_independent_series):
+            rounded_cvm_ind = round(cvm_ind_value, 3)
+            ax.annotate(rounded_cvm_ind, xy=(idx+0.05, cvm_ind_value+0.05), rotation=90)
+        # Display the final plot with legend
+        fig.tight_layout()
+        ax.legend()
+        plt.show()
+
+
+
