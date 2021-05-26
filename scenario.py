670cf24d42b3ad6147c4152ae5b9df1a8c346042
AlexandreLanglade <developjtaa@outlook.fr>
2021-05-26 12:25:15 +0200

    proposal 2 correction

--- scenario.py
+++ scenario.py
@@ -10,8 +10,8 @@ f = lambda x: np.random.randint(0,2)
 labels = [0, 1, 1, 0, 1, 0, 0, 0, 1, 1]
 
 my_problem = data_management.factory.create_fairness_problem(inputs=x, function=f, labels=labels)
-indices.cvm(my_problem, cols=2)
-visualization.one_way_to_display(my_problem)
+indices.cvm.compute_cvm(my_problem, cols=2)
+visualization.one_way_to_display.one_way_to_display(my_problem)
 
 
 
--- visualization/one_way_to_display.py
+++ visualization/one_way_to_display.py
@@ -1,5 +1,5 @@
 from fairness_problem import FairnessProblem
 
 def one_way_to_display(fairness_problem : FairnessProblem):
-    foo = FairnessProblem.get_result()
+    foo = fairness_problem.get_result()
     print("....")
