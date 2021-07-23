ac527f73e6b5c231a63abe858b71a7a032a7b5d4
AlexandreLanglade <developjtaa@outlook.fr>
2021-07-23 11:33:00 +0200

    push réu

--- tests/test_cvm.py
+++ tests/test_cvm.py
@@ -16,8 +16,10 @@ class TestCVM(unittest.TestCase):
         compute_cvm(my_problem)
         print(my_problem.get_result())
         visu_cvm(my_problem)
+        res_df = pd.DataFrame([["0.06766917293233082",0.0],[0.20300751879699247,0.0],[0.3082706766917293,0.0],[0.18796992481203006,0.0]],columns=["CVM","CVM_indep"])
         
-        #self.assertEqual()
+        print(my_problem.get_result().eq(res_df))
+        self.assertTrue(my_problem.get_result().equals(res_df))
 
 if __name__ == '__main__':
     unittest.main()
