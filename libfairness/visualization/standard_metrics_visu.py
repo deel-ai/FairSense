b6d93ec961cf87e6c2aecd3976458016e7edf9aa
AlexandreLanglade <developjtaa@outlook.fr>
2021-08-03 16:11:53 +0200

    disparate impact indice + visu fixed + add test

--- libfairness/visualization/standard_metrics_visu.py
+++ libfairness/visualization/standard_metrics_visu.py
@@ -5,6 +5,23 @@ from libfairness.fairness_problem import FairnessProblem
 
 def visu_disparate_impact(fairness_problem):
     sns.set_theme(style="white", context="notebook")
+    custom_palette = {}
+    res = fairness_problem.get_result().copy()
+    for elt in res:
+        res[elt] -= 1
+        if res[elt] >= 0 :
+            custom_palette[elt] = 'r'
+        else : 
+            custom_palette[elt] = 'g'
+    sns.barplot(x=list(res.keys()), y=list(res.values()), palette=custom_palette)
+    plt.axhline(0, color="k", clip_on=False)
+    sns.despine(bottom=True)
+    plt.tight_layout(h_pad=2)
+    plt.show()
+
+
+def old_visu_disparate_impact(fairness_problem):
+    sns.set_theme(style="white", context="notebook")
     custom_palette = sns.diverging_palette(150, 10, s=80, l=55, n=2)
     gs = fairness_problem.get_groups_studied()
     n = len(gs)
--- tests/testDI.py
+++ tests/testDI.py
@@ -1,23 +1,47 @@
-import libfairness.indices.standard_metrics
-import libfairness.visualization.standard_metrics_visu
-import numpy as np
+import unittest
+
 import libfairness.data_management.factory
+import libfairness.indices.standard_metrics
+import numpy as np
 
 
-# data
-inputs = np.array([[1., 0.], [1., 0.], [1., 1.], [1., 1.], [0., 1.], [1., 0.], [1., 1.], [0., 1.], [1., 1.], [
-                  1., 1.], [0., 0.], [0., 0.], [0., 1.], [1., 0.], [1., 1.], [1., 1.], [0., 1.], [1., 1.], [0., 0.], [1., 1.]])
-outputs = np.array([[0],[1],[0],[1],[1],[0],[1],[1],[0],[0],[0],[1],[0],[0],[1],[1],[1],[0],[1],[0]])
+class TestSobol(unittest.TestCase):
 
-columns = ["Male","Driver_licence"]
+    def test_disparate_impact(self):
 
-gs = [["Male"],["Driver_licence"]]
-#gs = [[0],[1]]
-my_problem = libfairness.data_management.factory.create_fairness_problem(inputs=inputs, outputs=outputs,groups_studied=gs,columns=columns)
-libfairness.indices.standard_metrics.compute_disparate_impact(my_problem)
-print(my_problem.get_result())
-libfairness.visualization.standard_metrics_visu.visu_disparate_impact(my_problem)
+        # data
+        inputs = np.array([[1., 0.], [1., 0.], [1., 1.], [1., 1.], [0., 1.], [1., 0.], [1., 1.], [0., 1.], [1., 1.], [
+            1., 1.], [0., 0.], [0., 0.], [0., 1.], [1., 0.], [1., 1.], [1., 1.], [0., 1.], [1., 1.], [0., 0.], [1., 1.]])
+        outputs = np.array([[0], [1], [0], [1], [1], [0], [1], [1], [0], [0], [
+                           0], [1], [0], [0], [1], [1], [1], [0], [1], [0]])
 
-result_one = {0: {1.0: 0.7692307692307693, 0.0: 1.4285714285714286}}
+        columns = ["Male", "Driver_licence"]
 
-t = {0: {1.0: 0.7692307692307693, 0.0: 1.4285714285714286}, 1: {0.0: 0.8571428571428571, 1.0: 1.0769230769230769}}
+        gs1 = [["Male"], ["Driver_licence"]]
+        gs2 = [[0], [1]]
+        gs3 = [[1]]
+
+        # results that we must obtain
+        result_hard = {'Male': 1.857142857142857,
+                       'Driver_licence': 0.7959183673469388}
+        result_hard_3 = {'Driver_licence': 0.7959183673469388}
+        # fairness problem
+        my_problem = libfairness.data_management.factory.create_fairness_problem(
+            inputs=inputs, outputs=outputs, groups_studied=gs1, columns=columns)
+        libfairness.indices.standard_metrics.compute_disparate_impact(
+            my_problem)
+        self.assertEqual(my_problem.get_result(), result_hard)
+
+        my_problem.set_groups_studied(gs2)
+        libfairness.indices.standard_metrics.compute_disparate_impact(
+            my_problem)
+        self.assertEqual(my_problem.get_result(), result_hard)
+
+        my_problem.set_groups_studied(gs3)
+        libfairness.indices.standard_metrics.compute_disparate_impact(
+            my_problem)
+        self.assertEqual(my_problem.get_result(), result_hard_3)
+
+
+if __name__ == '__main__':
+    unittest.main()
