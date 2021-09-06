eef1875829d62248bb9b175ec88e00a923ade0ed
thibaut.boissin <thibaut.boissin@irt-saintexupery.com>
2021-09-06 14:56:36 +0200

    tests for disparate impact

--- tests/testDI.py
+++ tests/testDI.py
@@ -1,86 +1,107 @@
 import unittest
 
 import libfairness.data_management.factory
-import libfairness.indices.standard_metrics
+from libfairness.indices.standard_metrics import disparate_impact
 import numpy as np
+import pandas as pd
+
+from libfairness.utils.dataclasses import IndicesInput
 
 
 class TestSobol(unittest.TestCase):
+    def test_new(self):
+        inputs = IndicesInput(
+            model=None,
+            x=pd.DataFrame(
+                data=np.random.randint(low=0, high=2, size=(5000, 4)),
+                columns="a,b,c," "" "d".split(","),
+            ),
+            y=pd.DataFrame(np.random.randint(low=0, high=2, size=(5000,))),
+            variable_groups=[["a", "b"], ["c"], ["d"]],
+        )
+        outputs = disparate_impact(inputs)
+        np.testing.assert_array_less(0.95, outputs.results)
+        print(outputs)
+
     def test_disparate_impact(self):
 
-        # data
-        inputs = np.array(
-            [
-                [1.0, 0.0],
-                [1.0, 0.0],
-                [1.0, 1.0],
-                [1.0, 1.0],
-                [0.0, 1.0],
-                [1.0, 0.0],
-                [1.0, 1.0],
-                [0.0, 1.0],
-                [1.0, 1.0],
-                [1.0, 1.0],
-                [0.0, 0.0],
-                [0.0, 0.0],
-                [0.0, 1.0],
-                [1.0, 0.0],
-                [1.0, 1.0],
-                [1.0, 1.0],
-                [0.0, 1.0],
-                [1.0, 1.0],
-                [0.0, 0.0],
-                [1.0, 1.0],
-            ]
-        )
-        outputs = np.array(
-            [
-                [0],
-                [1],
-                [0],
-                [1],
-                [1],
-                [0],
-                [1],
-                [1],
-                [0],
-                [0],
-                [0],
-                [1],
-                [0],
-                [0],
-                [1],
-                [1],
-                [1],
-                [0],
-                [1],
-                [0],
-            ]
-        )
-
         columns = ["Male", "Driver_licence"]
+        # data
+        inputs = pd.DataFrame(
+            np.array(
+                [
+                    [1.0, 0.0],
+                    [1.0, 0.0],
+                    [1.0, 1.0],
+                    [1.0, 1.0],
+                    [0.0, 1.0],
+                    [1.0, 0.0],
+                    [1.0, 1.0],
+                    [0.0, 1.0],
+                    [1.0, 1.0],
+                    [1.0, 1.0],
+                    [0.0, 0.0],
+                    [0.0, 0.0],
+                    [0.0, 1.0],
+                    [1.0, 0.0],
+                    [1.0, 1.0],
+                    [1.0, 1.0],
+                    [0.0, 1.0],
+                    [1.0, 1.0],
+                    [0.0, 0.0],
+                    [1.0, 1.0],
+                ]
+            ),
+            columns=columns,
+        )
+        outputs = pd.DataFrame(
+            np.array(
+                [
+                    [0],
+                    [1],
+                    [0],
+                    [1],
+                    [1],
+                    [0],
+                    [1],
+                    [1],
+                    [0],
+                    [0],
+                    [0],
+                    [1],
+                    [0],
+                    [0],
+                    [1],
+                    [1],
+                    [1],
+                    [0],
+                    [1],
+                    [0],
+                ]
+            ),
+            columns=["outputs"],
+        )
 
         gs1 = [["Male"], ["Driver_licence"]]
-        gs2 = [[0], [1]]
-        gs3 = [[1]]
+        gs2 = [["Driver_licence"]]
 
         # results that we must obtain
-        result_hard = {"Male": 1.857142857142857, "Driver_licence": 0.7959183673469388}
-        result_hard_3 = {"Driver_licence": 0.7959183673469388}
+        result_hard = pd.DataFrame(
+            {"Male": [0.538462], "Driver_licence": [0.795918]}
+        ).transpose()
+        result_hard_2 = pd.DataFrame({"Driver_licence": [0.795918]}).transpose()
         # fairness problem
-        my_problem = libfairness.data_management.factory.create_fairness_problem(
-            inputs=inputs, outputs=outputs, groups_studied=gs1, columns=columns
+        index_input = IndicesInput(x=inputs, y=outputs, variable_groups=gs1)
+        result = disparate_impact(index_input)
+        np.testing.assert_allclose(
+            result.results.values, result_hard.values, atol=1e-4, rtol=1e-4
         )
-        libfairness.indices.standard_metrics.compute_disparate_impact(my_problem)
-        self.assertEqual(my_problem.get_result(), result_hard)
 
-        my_problem.set_groups_studied(gs2)
-        libfairness.indices.standard_metrics.compute_disparate_impact(my_problem)
-        self.assertEqual(my_problem.get_result(), result_hard)
-
-        my_problem.set_groups_studied(gs3)
-        libfairness.indices.standard_metrics.compute_disparate_impact(my_problem)
-        self.assertEqual(my_problem.get_result(), result_hard_3)
+        index_input = IndicesInput(x=inputs, y=outputs, variable_groups=gs2)
+        result = disparate_impact(index_input)
+        np.testing.assert_allclose(
+            result.results.values, result_hard_2.values, atol=1e-4, rtol=1e-4
+        )
 
 
 if __name__ == "__main__":
