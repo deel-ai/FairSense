94b212d58e259ca9e5d1eb0df16cb0aa0dd6d237
thibaut.boissin <thibaut.boissin@irt-saintexupery.com>
2021-09-07 17:09:39 +0200

    fixed tests for disparate impact

--- tests/testDI.py
+++ tests/testDI.py
@@ -1,6 +1,4 @@
 import unittest
-
-import libfairness.data_management.factory
 from libfairness.indices.standard_metrics import disparate_impact
 import numpy as np
 import pandas as pd
@@ -20,7 +18,7 @@ class TestSobol(unittest.TestCase):
             variable_groups=[["a", "b"], ["c"], ["d"]],
         )
         outputs = disparate_impact(inputs)
-        np.testing.assert_array_less(0.95, outputs.results)
+        np.testing.assert_array_less(outputs.values, 0.05)
         print(outputs)
 
     def test_disparate_impact(self):
@@ -83,24 +81,19 @@ class TestSobol(unittest.TestCase):
         )
 
         gs1 = [["Male"], ["Driver_licence"]]
-        gs2 = [["Male"]]
-
-        # results that we must obtain
-        result_hard = pd.DataFrame(
-            {"Male": [0.538462], "Driver_licence": [0.795918]}
-        ).transpose()
-        result_hard_2 = pd.DataFrame({"Driver_licence": [0.795918]}).transpose()
+        gs2 = [["Driver_licence"]]
         # fairness problem
         index_input = IndicesInput(x=inputs, y=outputs, variable_groups=gs1)
         result = disparate_impact(index_input)
-        np.testing.assert_allclose(
-            result.values.values, result_hard.values, atol=1e-4, rtol=1e-4
+        self.assertAlmostEqual(result.values.loc["Male"]["DI"], 1 - 0.538462, places=4)
+        self.assertAlmostEqual(
+            result.values.loc["Driver_licence"]["DI"], 1 - 0.795918, places=4
         )
 
         index_input = IndicesInput(x=inputs, y=outputs, variable_groups=gs2)
         result = disparate_impact(index_input)
-        np.testing.assert_allclose(
-            result.values.values, result_hard_2.values, atol=1e-4, rtol=1e-4
+        self.assertAlmostEqual(
+            result.values.loc["Driver_licence"]["DI"], 1 - 0.795918, places=4
         )
 
 
