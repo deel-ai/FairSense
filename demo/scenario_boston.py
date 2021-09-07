55fc623c34b2ed029ff4fc8783447edad7f8a542
thibaut.boissin <thibaut.boissin@irt-saintexupery.com>
2021-09-07 15:42:38 +0200

    update scenario_boston.py

--- demo/scenario_boston.py
+++ demo/scenario_boston.py
@@ -1,11 +1,11 @@
 import sklearn.datasets as ds
-import pandas as pd
-from libfairness.data_management.factory import from_pandas, from_numpy
+from libfairness.data_management.factory import from_numpy
 from libfairness.data_management.processing import one_hot_encode
+from libfairness.indices.confidence_intervals import with_confidence_intervals
 from libfairness.indices.cvm import cvm_indices
 from libfairness.indices.standard_metrics import disparate_impact
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     # load data
     data = ds.load_boston()
     # construct IndicesInput object
@@ -13,5 +13,7 @@ if __name__ == '__main__':
     # apply one hot encoding
     indices_inputs = one_hot_encode(indices_inputs, ["CHAS", "RAD"])
     # compute indices
-    indices_outputs = disparate_impact(indices_inputs) + cvm_indices(indices_inputs)
-    print(indices_outputs.results)
+    indices_outputs = with_confidence_intervals(n_splits=31)(disparate_impact)(
+        indices_inputs
+    ) + with_confidence_intervals(n_splits=31)(cvm_indices)(indices_inputs)
+    print(indices_outputs.values)
