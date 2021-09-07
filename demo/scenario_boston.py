3cb2a40624db3ff5ca16187d92e064229f6352d7
thibaut.boissin <thibaut.boissin@irt-saintexupery.com>
2021-09-07 16:54:43 +0200

    update scenario_boston.py

--- demo/scenario_boston.py
+++ demo/scenario_boston.py
@@ -1,9 +1,11 @@
 import sklearn.datasets as ds
+import matplotlib.pyplot as plt
 from libfairness.data_management.factory import from_numpy
 from libfairness.data_management.processing import one_hot_encode
 from libfairness.indices.confidence_intervals import with_confidence_intervals
 from libfairness.indices.cvm import cvm_indices
 from libfairness.indices.standard_metrics import disparate_impact
+from libfairness.visualization.plots import bar_plot
 
 if __name__ == "__main__":
     # load data
@@ -16,4 +18,5 @@ if __name__ == "__main__":
     indices_outputs = with_confidence_intervals(n_splits=31)(disparate_impact)(
         indices_inputs
     ) + with_confidence_intervals(n_splits=31)(cvm_indices)(indices_inputs)
-    print(indices_outputs.values)
+    ax = bar_plot(indices_outputs, plot_per="index", kind="box")
+    plt.show()
