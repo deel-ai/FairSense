45938eddba7491d1ed1e1a5675eeb884dd710c77
thibaut.boissin <thibaut.boissin@irt-saintexupery.com>
2021-10-20 16:10:34 +0200

    documentation of viz methods

--- libfairness/visualization/plots.py
+++ libfairness/visualization/plots.py
@@ -1,10 +1,35 @@
 import seaborn as sns
 from libfairness.utils.dataclasses import IndicesOutput
+"""
+This module contains functions used to plot the outputs of the indices. The outputs of 
+the indices are epresented as :mod:`.utils.dataclasses.IndicesOutput`.
+"""
 
 
 def cat_plot(
     indices: IndicesOutput, plot_per="variable", kind="bar", col_wrap=None, **kwargs
 ):
+    """
+    Uses the `seaborn.catplot`_ to plot the indices.
+
+    Args:
+        indices (IndicesOutput): computed indices
+        plot_per (str): can be either `variable` or `index`, when set to `variable`
+            there is one graph per variable, each graph showing the values of all
+            indices. Respectively setting to `index` will build one graph per index,
+            each showing the values for all variable.
+        kind (str): kind of visualization to produce, can be one of `strip`, `swarm`,
+            `box`, `violin`, `boxen`, `point`, `bar`.
+        col_wrap (Optional(int)): “Wrap” the column variable at this width, so that
+            the column facets span multiple rows.
+        **kwargs: extra arguments passed to `seaborn.catplot`_.
+
+    .. _seaborn.catplot:
+        https://seaborn.pydata.org/generated/seaborn.catplot.html#seaborn.catplot
+
+    Returns: a matplotlib axes object
+
+    """
     assert plot_per.lower().strip() in {"variable", "index", "indices"}
     if plot_per == "variable":
         col = "variable"
--- libfairness/visualization/text.py
+++ libfairness/visualization/text.py
@@ -2,8 +2,28 @@ import numpy as np
 from functools import partial
 from libfairness.utils.dataclasses import IndicesOutput
 
+"""
+This module contains functions used to visualize the outputs of the indices in text 
+format. The outputs of the indices are epresented as 
+:mod:`.utils.dataclasses.IndicesOutput`.
+"""
+
 
 def format_with_intervals(indices_outputs: IndicesOutput, quantile: float = 0.05):
+    f"""
+    Pretty print the indices table with confidence intervals. Note that the intervals
+    are displayed even if the indices are computed without confidence intervals. See
+    :mod:`libfairness.indices.confidence_intervals` for more information.
+
+    Args:
+        indices_outputs (IndicesOutput): computed indices
+        quantile (float): quantile used to compute confidence intervals. Values must
+        be in [0., 0.5].
+
+    Returns: the table with indices properly displayed. Note that the table values
+        are now string and not float.
+
+    """
     means = indices_outputs.runs.groupby(level=0).median().clip(0.0, 1.0)
     low = (
         indices_outputs.runs.groupby(level=0)
